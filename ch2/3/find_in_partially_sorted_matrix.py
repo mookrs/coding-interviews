ec61403c01c2358622c221013622fa4d6263a0b3
Mookrs <mookrs@gmail.com>
2018-08-18 14:24:16 +0800

    Add problem 2 & 3

---
+++ ch2/3/find_in_partially_sorted_matrix.py
@@ -0,0 +1,21 @@
+def find(matrix, number):
+    if not matrix:
+        return False
+    rows, cols = len(matrix), len(matrix[0])
+    row, col = 0, cols - 1
+    while row < rows and col >= 0:
+        if matrix[row][col] == number:
+            return True
+        elif matrix[row][col] > number:
+            col -= 1
+        else:
+            row += 1
+
+    return False
+
+
+matrix = [[1, 2, 3],
+          [2, 3, 6],
+          [3, 6, 7]]
+number = 6
+print(find(matrix, number))
