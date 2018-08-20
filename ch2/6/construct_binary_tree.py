8726b21e7018468690b974c0596989b84c489f7d
Mookrs <mookrs@gmail.com>
2018-08-20 15:21:27 +0800

    Fix using `-` slice bug

--- ch2/6/construct_binary_tree.py
+++ ch2/6/construct_binary_tree.py
@@ -65,14 +65,11 @@ class Tree(object):
 
 def construct_tree(preorder=None, inorder=None):
     if not preorder or not inorder:
-        # TODO 有点不理解 or
         return None
-    index = inorder.index(preorder[0])
-    left = inorder[0:index]
-    right = inorder[index + 1:]
     root = TreeNode(preorder[0])
-    root.left = construct_tree(preorder[1:1 + len(left)], left)
-    root.right = construct_tree(preorder[-len(right):], right)
+    i = inorder.index(preorder[0])
+    root.left = construct_tree(preorder[1:i + 1], inorder[0:i])
+    root.right = construct_tree(preorder[i + 1:], inorder[i + 1:])
     return root
 
 
