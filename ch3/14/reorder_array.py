21a5893a343ec65f15529f072a6344b1b9d86a0e
Mookrs <mookrs@gmail.com>
2018-08-23 14:14:35 +0800

    Add problem 14

---
+++ ch3/14/reorder_array.py
@@ -0,0 +1,18 @@
+def reorder(nums, func):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        while not func(nums[left]):
+            left += 1
+        while func(nums[right]):
+            right -= 1
+        if left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+
+
+def is_even(num):
+    return (num & 1) == 0
+
+
+tests = [2, 3, 4, 5, 6, 7]
+reorder(tests, is_even)
+print(tests)
