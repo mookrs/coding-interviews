ba1340862f384f7dbea7280c64e7374f1dcc36e9
Mookrs <mookrs@gmail.com>
2018-09-20 22:40:36 +0800

    Reformat code

--- ch2/3/find_in_partially_sorted_matrix.py
+++ ch2/3/find_in_partially_sorted_matrix.py
@@ -5,12 +5,12 @@ def find(matrix, number):
     rows, cols = len(matrix), len(matrix[0])
     row, col = 0, cols - 1
     while row < rows and col >= 0:
-        if matrix[row][col] == number:
-            return True
+        if matrix[row][col] < number:
+            row += 1
         elif matrix[row][col] > number:
             col -= 1
         else:
-            row += 1
+            return True
 
     return False
 
